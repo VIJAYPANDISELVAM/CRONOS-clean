08c0702862f1e7dbebb8da8c0e150acf9f1e0f6a
VIJAYPANDISELVAM <vijaypandiselvam01@gmail.com>
2026-01-09 20:02:55 +0530

    Delete analyzer directory

--- analyzer/rule_matcher.py
+++
@@ -1,5 +0,0 @@
-def match_rules(all_rules, target_variable):
-    """
-    Returns all rules that match the same variable
-    """
-    return [r for r in all_rules if r["variable"] == target_variable]
